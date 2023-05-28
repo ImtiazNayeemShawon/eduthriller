aff82761a2441712da45250390b02e51bd6cb67e
imtiaz <imtiaznayim@gmail.com>
2023-05-28 20:01:44 +0600

    backend integretation

--- pages/api/apiCaller.js
+++ pages/api/apiCaller.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const apiCaller = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your actual base URL
+  baseURL: 'https://zip-mt2o.onrender.com/', // Replace with your actual base URL
 });
 
 export default apiCaller;
