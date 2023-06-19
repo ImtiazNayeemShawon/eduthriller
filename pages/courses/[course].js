9a57f13f737c8ce8dcc8d2e5f9993d518228bc2d
imtiaz <imtiaznayim@gmail.com>
2023-06-19 23:14:06 +0600

    asdsadsa

--- pages/courses/[course].js
+++ pages/courses/[course].js
@@ -180,12 +180,12 @@ export default function Course() {
               <div>
                 {Data.micros.map((micro, index) => (
                   <ul key={index} className="mt-2">
-                    <li className="mt-1  text-md flex gap-4">
+                    <li className="mt-0 text-gray-700 text-md flex gap-1 bangfont">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
                         fill="currentColor"
-                        className="w-6 h-6"
+                        className="w-6 h-6 text-green-600"
                       >
                         <path
                           fillRule="evenodd"
@@ -193,9 +193,9 @@ export default function Course() {
                           clipRule="evenodd"
                         />
                       </svg>
-
-                      {micro.number}
-                      {micro.title}
+                      <span>{micro.number}</span>
+                      <span> টি</span>
+                      <span>{micro.title}</span>
                     </li>
                   </ul>
                 ))}
@@ -300,21 +300,22 @@ export default function Course() {
                 <div>
                   {Data.micros.map((micro, index) => (
                     <ul key={index} className="mt-4">
-                      <li className="mt-0  text-md flex gap-4 bangfont">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="w-6 h-6 text-green-600"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                       <span className=""> {micro.number}</span> 
-                        {micro.title}
+                      <li className="mt-0 text-gray-700 text-md flex gap-1 bangfont">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="w-6 h-6 text-green-600"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                        <span>{micro.number}</span>
+                        <span> টি</span>
+                        <span>{micro.title}</span>
                       </li>
                     </ul>
                   ))}
