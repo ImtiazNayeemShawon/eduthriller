5894309be2aee26524904e628aee8fd914bf5968
imtiaz <imtiaznayim@gmail.com>
2023-07-06 00:21:52 +0600

    side png added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  distDir: "build",
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
   images: {
     domains: ['i.ibb.co',],
